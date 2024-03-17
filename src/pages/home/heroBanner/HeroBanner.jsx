f2ebe4e58f088f7b85c8f84cac41a953e75462db
wasim <wasimakhter2030@gmail.com>
2024-03-17 16:40:43 +0530

    add initial setup

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,15 @@
+import "./style.scss";
+
+const HeroBanner = () => {
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
